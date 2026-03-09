c6611d9db7ee1d2ff653c19a2c1cbf11526418d7
KevinU20221275 <kevinmontanodev@gmail.com>
2026-03-08 20:05:44 -0600

    feat: add turn api route

---
+++ src/pages/api/battle/turn.ts
@@ -0,0 +1,18 @@
+import { BATTLE_WITH_FAINT_SWITCH, battleTurnMock } from "@/mocks/battleTurn";
+import { handleApiError } from "@/server/errors/handleApiError";
+import { battleTurn } from "@/server/services/battle.service";
+import type { APIRoute } from "astro";
+
+export const POST:APIRoute = async ({locals, request}) => {
+    try {
+        const token = locals.token
+        
+        if (!token) return Response.json({message: "Unahutorize"}, {status: 404})
+        
+        const body = await request.json()
+
+        return Response.json(await battleTurn(body, token))
+    } catch (error) {
+        return handleApiError(error)
+    }
+}
