ec0a57d75db84eb713c4a838b2fd2eb7ce19b58e
KevinU20221275 <kevinmontanodev@gmail.com>
2026-03-18 12:31:06 -0600

    debug: debug auth vercel routes error

--- src/pages/api/pokedex/evolve/[pokemonId].ts
+++ src/pages/api/pokedex/evolve/[pokemonId].ts
@@ -4,6 +4,10 @@ import type { APIRoute } from "astro";
 
 export const POST:APIRoute = async ({locals, params}) => {
     try {
+        console.log("TOKEN:", locals.token)
+        if (!locals.token){
+            return Response.json({message: "Unhautorize"}, {status: 403})
+        }
         const pokemonId = params.pokemonId
         return Response.json(await evolPokemon(locals.token!, pokemonId!))
     } catch (error) {
